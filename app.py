eb9cc90b3a7d6480476c1b44d284d44258e261a1
Mark Fleckser <mfleckser@gmail.com>
2020-03-18 10:39:58 -0400

    added alert messages and started form requests

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = "sdfj_+183&4/.>"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 db = SQLAlchemy(app)
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
-	username = db.Column(db.String(30), nullable = False)
-	name = db.Column(db.String(30), nullable = False)
-	password = db.Column(db.String(30), nullable = False)
+	username = db.Column(db.String(64), nullable = False, unique = True)
+	name = db.Column(db.String(64), nullable = False)
+	password_hash = db.Column(db.String(128))
 	platformer = db.Column(db.Integer, nullable = False, default = 9999999999999)
 	minesweeper = db.Column(db.Integer, nullable = False, default = 9999999999999)
 	space_invaders = db.Column(db.Integer, nullable = False, default = -1)
@@ -18,13 +20,24 @@ class User(db.Model):
 	def __repr__(self):
 		return "<User %r>" % self.id
 
-@app.route("/")
-def Start():
-	return render_template("Start.html")
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
 
-@app.route("/signup")
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+@app.route("/",methods=["POST","GET"])
+def Start():
+	if request.method == "POST":
+		pass
+	flash("test")
+	return render_template("Start.html",failed=False)
+
+@app.route("/signup",methods=["POST","GET"])
 def signup():
-	return render_template("signup.html")
+	if request.method == "POST":
+		pass
+	return render_template("signup.html",message="")
 
 @app.route("/calculator")
 def calculator():
